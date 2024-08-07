4d76d5514ddaddaf806e6edebf149453625261df
Necati Alperen Işık <necatialperen2@gmail.com>
2024-08-07 23:59:01 +0300

    bug fix

--- Sources/UIFactoryKit/CommonExtensions.swift
+++ Sources/UIFactoryKit/CommonExtensions.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Necati Alperen IŞIK on 7.08.2024.
 //
@@ -9,20 +9,20 @@ import UIKit
 
 // Using UI color with hex code
 
-extension UIColor {
+public extension UIColor {
     convenience init(hex: String) {
         var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
         hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-
+        
         var rgb: UInt64 = 0
-
+        
         Scanner(string: hexSanitized).scanHexInt64(&rgb)
-
+        
         let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
         let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
         let blue = CGFloat(rgb & 0x0000FF) / 255.0
         let alpha = CGFloat(1.0)
-
+        
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
@@ -30,7 +30,7 @@ extension UIColor {
 // Hide Keyboard
 // You must call it in the view did load function of the screen you want to hide the keyboard "self.hideKeyboardWhenTappedAround()"
 
-extension UIViewController {
+public extension UIViewController {
     func hideKeyboardWhenTappedAround() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
         tap.cancelsTouchesInView = false
@@ -45,11 +45,11 @@ extension UIViewController {
 // Removing HTML Tags from text
 // example : print(text.stripHTML)
 
-extension String {
+public extension String {
     var stripHTML: String {
         return self.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression)
     }
 }
 
 
-// 
+//
